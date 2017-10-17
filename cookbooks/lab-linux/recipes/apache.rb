045d43afe6e723525eff1dd8dbf7761b3f6db754
root <root@ip-172-31-29-152.ap-south-1.compute.internal>
2017-10-17 11:45:33 +0000

    all the messages

---
+++ cookbooks/lab-linux/recipes/apache.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: lab-linux
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+#package "apache2" do
+ #  action :install
+#end
