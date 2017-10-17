214cd30e52b3f5220c754bf38b52feb217399ebc
root <root@ip-172-31-29-152.ap-south-1.compute.internal>
2017-10-17 14:12:48 +0000

    robertcookbook

---
+++ cookbooks/robert/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: robert
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "maven" do
+
+  action: install
+
+end
+
