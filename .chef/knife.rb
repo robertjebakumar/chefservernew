0b515fd7dd290d7838a4274712fbe8cbc23f730a
root <root@ip-172-31-27-160.ap-south-1.compute.internal>
2017-10-25 08:14:40 +0000

    changes in receipes

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,7 @@ node_name                'robert'
 client_key               '/root/chef-repo/.chef/robert.pem'
 validation_client_name   'dengine-validator'
 validation_key           '/root/chef-repo/.chef/devops.pem'
-chef_server_url          'https://ec2-13-126-199-118.ap-south-1.compute.amazonaws.com/organizations/dengine'
+chef_server_url          'https://ec2-13-126-21-110.ap-south-1.compute.amazonaws.com/organizations/dengine'
 cookbook_path            ["/root/chef-repo/cookbooks"]
 syntax_check_cache_path  '/root/syntax_cache'
 ssl_verify_mode    :verify_none
@@ -15,8 +15,8 @@ knife[:editor]   = "vi"
 knife[:ssh_user] = 'ubuntu'
 # The data required by knife to authenticate with AWS console/account
 #knife[:aws_credential_file] = '/root/chef-repo/.chef/credentials/aws_credential_file'
-knife[:aws_access_key_id]     = 'AKIAJEDDKWYENOGO4Q7Q'
-knife[:aws_secret_access_key] = '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'
-knife[:identity_file]         = '/root/chef-repo/.chef/clientmachines.pem'
-knife[:ssh_key_name]          = 'clientmachines'
+knife[:aws_access_key_id]     = 'AKIAITLEPMF3OADJAPYQ'
+knife[:aws_secret_access_key] = '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'
+knife[:identity_file]         = '/root/chef-repo/.chef/keypairclient.pem'
+knife[:ssh_key_name]          = 'keypairclient'
 knife[:region]                = 'ap-south-1'
